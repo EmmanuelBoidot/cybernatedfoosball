6ec7abab3811fc6e6fe70dace9de628c818b7353
Emmanuel <eboidot3@gatech.edu>
2016-10-19 02:53:13 -0700

    plop

--- league/db/lists/live.js
+++ league/db/lists/live.js
@@ -1,1 +1,24 @@
-function(head, req) {  start({'headers': {'content-type': 'text/html'}});  var mustache = require('lib/mustache'),      template = this.templates['live.html'],      config = this.config,      locales = this.resources['locales_' + config.locale];  var data = {    scoreboard: config.scoreboard,    players: []  };  for (var key in locales) {    data['locales.' + key] = locales[key];  }    var row;  while (row = getRow()) {    data.players.push(row.value);  }  data.players.sort(function(a, b) {    return (a.score > b.score) ? -1 : ((a.score < b.score) ? 1 : 0);  });  data.players.length > 10 && (data.players.length = 10);  var pos = 0,      prev;  for (var i = 0; i < data.players.length; ++i) {    var player = data.players[i];    ++pos;    if (player.score != prev) {      player.position = pos;    }    prev = player.score;  }  return mustache.to_html(template, data);};
+function(head, req) {
+    start({ 'headers': { 'content-type': 'text/html' } });
+    var mustache = require('lib/mustache'),
+        template = this.templates['live.html'],
+        config = this.config,
+        locales = this.resources['locales_' + config.locale];
+    var data = { scoreboard: config.scoreboard, players: [] };
+    for (var key in locales) { data['locales.' + key] = locales[key]; }
+    var row;
+    while (row = getRow()) { data.players.push(row.value); }
+    data.players.sort(function(a, b) {
+        return (a.score > b.score) ? -1 : ((a.score < b.score) ? 1 : 0);
+    });
+    data.players.length > 10 && (data.players.length = 10);
+    var pos = 0,
+        prev;
+    for (var i = 0; i < data.players.length; ++i) {
+        var player = data.players[i];
+        ++pos;
+        if (player.score != prev) { player.position = pos; }
+        prev = player.score;
+    }
+    return mustache.to_html(template, data);
+};
